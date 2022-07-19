1e65b9b15058fb50156fcf37d6897951c33d3e36
VSF <47865897+HashiY@users.noreply.github.com>
2022-07-19 16:29:50 -0300

    Added Scoreboard
    Changed the force in y
    Added Scoreboard

--- Aviao/Assets/Script/ControlaJogador.cs
+++ Aviao/Assets/Script/ControlaJogador.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ControlaJogador : MonoBehaviour {
 
+	private string sceneName;
 	private bool comecouJogo;
 	private bool acabouJogo; 
-	private int pontuacao;
-	public Text textoScore;
+	private int score, highscore;
+	public Text scoreText;
+	public Text startText;
+	public Text entryNameText;
+	public Text entryScoreText;
+	public GameObject objetoCanvas;
+	public GameObject objetoCanvasHighscore;
 	GameObject objetoGameEngine;
+	RectTransform rectTransform;
 
-	Vector2 forcaImpulso = new Vector2(0,500);
+	Vector2 forcaImpulso = new Vector2(0,300); //
 
 	public GameObject objetoParticulasPenas;
  
@@ -18,24 +26,43 @@ public class ControlaJogador : MonoBehaviour {
 		objetoGameEngine = GameObject.FindGameObjectWithTag("MainCamera");
 
 		float larguraTela = (Camera.main.orthographicSize*2f)/Screen.height*Screen.width;   
-		transform.position = new Vector2(-larguraTela/4,0f); 
- 
-		textoScore.transform.position = new Vector2(Screen.width/2,Screen.height - 250);  //onde colocar
-		textoScore.text = "Toque para iniciar!";
-		textoScore.fontSize = 35; 
+		transform.position = new Vector2(-larguraTela/4,0f);
+
+		startText.transform.position = new Vector2(Screen.width / 2, Screen.height - 100);
+		startText.text = "Toque para iniciar!";
+		startText.fontSize = 35;
+		startText.color = new Color(0.95f, 1.0f, 0.35f);
+
+		objetoCanvas.transform.position = new Vector2(Screen.width/6 - 50, Screen.height/6);
+		scoreText.text = "0";
+		scoreText.fontSize = 40;
+		scoreText.color = new Color(0.95f, 1.0f, 0.35f);
+		rectTransform = scoreText.GetComponent<RectTransform>();
+		rectTransform.sizeDelta = new Vector2(400, 200);
+
+		objetoCanvasHighscore.transform.position = new Vector2(Screen.width/6 - 50, Screen.height/6 - 50);
+		entryNameText.color = new Color(0.95f,1.0f,0.35f);
+		entryScoreText.color = new Color(0.95f,1.0f,0.35f);
+		entryScoreText.fontSize = 40;
+
+		//Colocando o highscore para ser salvo
+		sceneName = SceneManager.GetActiveScene().name;
+		if(PlayerPrefs.HasKey(sceneName + "score"))
+        {
+			highscore = PlayerPrefs.GetInt(sceneName + "score");
+			entryScoreText.text = highscore.ToString();
+		}
 	} 
 
 	void Update () { 
 		if(!acabouJogo){
-			if(Input.GetButtonDown("Fire1")){  //mause
+			if (Input.GetButtonDown("Fire1")){  //mause
 				if(!comecouJogo){ // se nao começou
 					comecouJogo=true;
 					GetComponent<Rigidbody2D>().isKinematic = false; // desliga
 
-                    textoScore.transform.position = new Vector2(Screen.width / 3, Screen.height - 150);
-                    textoScore.text = pontuacao.ToString(); // transformando para string
-					textoScore.fontSize = 50;
-					textoScore.color = new Color(0.95f,1.0f,0.35f);
+					startText.text = "";
+					scoreText.text = score.ToString(); // transformando para string
                     objetoGameEngine.SendMessage("Comeca");
                 }
 				GameObject particula = Instantiate(objetoParticulasPenas); // aparece as penas da prefab
@@ -77,9 +104,16 @@ public class ControlaJogador : MonoBehaviour {
 	} 
 
 	void Pontua(){
-		pontuacao++;
-		textoScore.text = pontuacao.ToString(); // atualizar a pontuaçao
-		print(pontuacao);
+		score++;
+		scoreText.text = score.ToString(); // atualizar a pontuaçao
+
+		if (score > highscore)
+		{
+			print(score);
+			highscore = score;
+			entryScoreText.text = highscore.ToString();
+			PlayerPrefs.SetInt(sceneName + "score", highscore);
+		}
 	}
 
 	void RecarregaCena(){
